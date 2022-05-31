7bffad1be0038bd01c7ad02a8f94bb70574ee382
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-31 10:42:25 -0300

    feat: start ViewController at Scene Delegate

---
+++ popularMovies/popularMovies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  popularMovies
+//
+//  Created by Grazielli Berti on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
---
+++ popularMovies/popularMoviesUITests/popularMoviesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  popularMoviesUITestsLaunchTests.swift
+//  popularMoviesUITests
+//
+//  Created by Grazielli Berti on 31/05/22.
+//
+
+import XCTest
+
+class popularMoviesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
